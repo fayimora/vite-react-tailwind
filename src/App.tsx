0449372a8a2898cfd29d346dbac06e6e061d3404
Fayi FB <fayi@fayimora.com>
2023-02-08 19:15:23 +0000

    Cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,28 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
+import React from "react";
 import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0);
-
+function Header() {
   return (
-    <div>
-      <h1>Hello world</h1>
+    <div className="flex items-center justify-between">
+      <h5>Canton Wallet</h5>
+      <div className="flex items-center space-x-4">
+        <a href="/#">Transactions</a>
+        <a href="/#">Subscriptions</a>
+        <a href="/#">Settings</a>
+        <a href="/#">Help</a>
+      </div>
+      <div className="flex items-center space-x-1 space-x-1">
+        <p>alice.cns</p>
+        <a href="/#">Logout</a>
+      </div>
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <div className="container max-w-full bg-blue-900">
+      <Header />
     </div>
   );
 }
